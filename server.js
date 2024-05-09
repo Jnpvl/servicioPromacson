7b1562ce6b496270ad0b6b23f92f054669f65ace
Jnpvl <151414138+Jnpvl@users.noreply.github.com>
2024-05-09 11:07:40 -0700

    carpetapublic

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
-
 const express = require('express');
 const cors = require('cors');
-const userRoutes = require('./api/userRoutes'); 
-const loginRoute = require('./api/loginRoute')
+const userRoutes = require('./api/userRoutes');
+const loginRoute = require('./api/loginRoute');
 const orderRoutes = require('./api/orderRoutes');
 
 const app = express();
-const port = 3001;
+const port = 3002;
 
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static('public'));
 
+// Rutas API
 app.use('/api/users', userRoutes);
 app.use('/api/login', loginRoute);
 app.use('/api/orders', orderRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/public/index.html');
 });
-
-//app.listen(port, () => {
-//  console.log(`Server listening at http://localhost:${port}`);
-//});
-
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server listening at http://192.168.1.72:${port}`);
@@ -33,6 +29,3 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
 });
-
-
-
